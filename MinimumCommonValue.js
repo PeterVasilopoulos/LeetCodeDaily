ef20ed80e2c220574f92ce29713874370fe6d51d
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-03-09 17:12:14 -0500

    created file

---
+++ MinimumCommonValue.js
@@ -0,0 +1,9 @@
+var getCommon = function(nums1, nums2) {
+    // object to hold occurences of values
+    let values = {}
+
+    // loop through both arrays
+    for(let i = 0; i < nums1.length || nums2.length; i++) {
+        
+    }
+}
