bbaeacfa53a27582fb291316cc6558705d2e02de
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-03-11 16:05:19 -0400

    created new file

---
+++ CustomSortString.js
@@ -0,0 +1,12 @@
+const order1 = 'cba'
+const s1 = 'abcd'
+
+const order2 = 'bcafg'
+const s2 = 'abcd'
+
+function customSortString(order, s) {
+    
+}
+
+console.log(customSortString(order1, s1))
+console.log(customSortString(order2, s2))
