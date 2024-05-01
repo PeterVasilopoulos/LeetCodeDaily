de83568a1b968e1e74164f8a581d57c1a6c0b3e6
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-04-30 20:02:52 -0400

    algo completed

--- NthTribonacciNumber.js
+++ NthTribonacciNumber.js
@@ -1,1 +1,15 @@
-function tribonacci() {}
+function tribonacci(n) {
+    let arr = new Array(n + 1);
+
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 1;
+
+    for (let i = 3; i < arr.length; i++) {
+        arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+    }
+
+    return arr[n];
+}
+
+console.log(tribonacci(25));
