66a0d04e7d7a13ef95b8306f70f4d8f2cbfd2d4c
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-09-12 10:18:37 -0400

    added comments

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -3,18 +3,24 @@ const num2 = -121
 const num3 = 10
 
 function isPalindrome(x) {
+    // check if number is negative
     if (x < 0) {
         return false
     }
 
+    // convert number to string
     const str = x.toString()
 
+    // loop through string forwards and backwards
     for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+        // check if nums are the same
         if (str[i] !== str[j]) {
+            // return false is nums are different
             return false
         }
     }
 
+    // return true
     return true
 }
 
